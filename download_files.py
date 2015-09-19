e9efc7a1858b439ae00e922e783d9c16ec10a8d4
Allen <dahanhbm@163.com>
2015-09-19 17:31:43 +0800

    Update download_files.py

--- download_files.py
+++ download_files.py
@@ -8,12 +8,13 @@ import urllib2
 #global vars
 process_count=0
 total_count=0
+failed_count=0
 
-#download files thread
-def download_files(locker,thread_id,url_list,cache_dir,timeout):
+def download_files_cb(locker,thread_id,url_list,cache_dir,timeout):
 	#used global vars
 	global process_count
 	global total_count
+	global failed_count
 	locker.acquire()
 	print("thread[%d] begin , len of url_list is %d , cache_dir is %s , timeout is %d ." % (thread_id,len(url_list),cache_dir,timeout))
 	locker.release()
@@ -36,7 +37,7 @@ def download_files(locker,thread_id,url_list,cache_dir,timeout):
 			file.close()	
 			response.close()
 		except:
-			pass
+			failed_count+=1
 	#function done!
 	locker.acquire()
 	print("thread[%d] end." % (thread_id))
@@ -46,48 +47,59 @@ def main_process(argv):
 	#used global vars
 	global process_count
 	global total_count
-	if len(argv) != 4:
-		print("usage:\n\t"+argv[0]+" [thread_num] [input_url_list_file] [output_images_dir_path]")
+	global failed_count
+	if len(argv) != 5:
+		print("usage:\n\t"+argv[0]+" [thread_num] [timeout(second)] [input_url_list_file] [output_files_dir_path]")
 		sys.exit()
 	#get parameters
 	thread_num = 0
+	timeout = 0
 	try:
 		thread_num = int(argv[1],10)
+		timeout = int(argv[2],10) 
 	except:
 		pass
-	input_url_list_file = argv[2]
-	output_images_dir_path = argv[3]
+	input_url_list_file = argv[3]
+	output_files_dir_path = argv[4]
 	locker = thread.allocate_lock()
-	timeout = 10
 	#check parameters
 	if thread_num<=0:
 		print("thread_num must be larger than zero!")
 		sys.exit()
+	if timeout<=0:
+		print("timeout must be larger than zero!")
+		sys.exit()
 	if not os.path.exists(input_url_list_file) or not os.path.isfile(input_url_list_file):
 		print("input_url_list_file is invalidate!")
 		sys.exit()
-	if not os.path.exists(output_images_dir_path) or not os.path.isdir(output_images_dir_path):
-		print("output_images_dir_path is invalidate!")
+	if not os.path.exists(output_files_dir_path) or not os.path.isdir(output_files_dir_path):
+		print("output_files_dir_path is invalidate!")
 		sys.exit()
 	#do action
 	urls = []
+	print("loading urls from input file...")
 	file=open(input_url_list_file)
 	for line in file:
 		urls.append(line.strip())
 	file.close()
+	print("load urls success")
+	raw_total_count=len(urls)
+	print("before removal of duplication , total count is %d." % (raw_total_count))
+	urls = list(set(urls))
 	total_count=len(urls)
-	print("total lines:"+str(total_count))
+	print("after removal of duplication , total count is %d." % (total_count))
 	if total_count<thread_num:
 		thread_num=1
-	count_per_thread=total_count/thread_num
+	count_per_thread=total_count/thread_num+1
 	try:
 		for i in range(0,thread_num):
 			start_pos=i*count_per_thread
 			stop_pos=start_pos+count_per_thread
 			if stop_pos>total_count:
 				stop_pos=total_count
+			print("thread[%d] will process [%d,%d)" % (i,start_pos,stop_pos))
 			urls_per_thread=urls[start_pos : stop_pos]
-			thread.start_new_thread(download_files,(locker,i,urls_per_thread,output_images_dir_path,timeout))
+			thread.start_new_thread(download_files_cb,(locker,i,urls_per_thread,output_files_dir_path,timeout))
 	except Exception as e:
 		print e
 		sys.exit()
@@ -96,6 +108,7 @@ def main_process(argv):
 			break
 		pass
 	print("all done")
+	print("total task:%d ,validate task:%d, success task:%d , failed task:%d" % (raw_total_count,total_count,total_count-failed_count,failed_count))
 	
 if __name__ == "__main__":
 	try:
@@ -103,6 +116,5 @@ if __name__ == "__main__":
 	except Exception as e:
 		print(e)
 	except KeyboardInterrupt:
-		pass
+		print("user canceld task!")
 	print("\nexit script!")
-
