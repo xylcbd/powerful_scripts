64f54d514a2a004290790231489933cb03f6059b
Allen <dahanhbm@163.com>
2015-09-25 15:25:01 +0800

    Update random_checkou.sql

--- random_checkou.sql
+++ random_checkou.sql
@@ -1,3 +1,3 @@
 SELECT url FROM images
- WHERE platform=2 AND id >= (SELECT FLOOR( RAND() * ((SELECT MAX(id) FROM images_20150909_feeds)-(SELECT MIN(id) FROM images_20150909_feeds)) + (SELECT MIN(id) FROM images_20150909_feeds)))    
+ WHERE platform=2 AND id >= (SELECT FLOOR( RAND() * ((SELECT MAX(id) FROM images)-(SELECT MIN(id) FROM images)) + (SELECT MIN(id) FROM images)))    
  ORDER BY id LIMIT 0,200;
